92fada7a05bc6e4a07630a6173f2112a6b58236b
Przemyslaw Gembalczyk <gembuspol@interia.pl>
2026-01-20 12:14:00 +0100

    a

---
+++ 23cz2/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hurtownia szkolna</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+<body>
+    <header>
+        <h1>Hurtownia z najlepszymi cenami</h1>
+    </header>
+    <section id="lewy">
+    <h2>Nasze ceny</h2>    
+    <!-- tutaj skrypt wypisujący tabele wykonany sanodzielnie -->
+    </section>
+    <section id="srodkowy">
+        <h2>Koszt zakupów</h2>
+        <form action="index.php" method="POST">
+            <label>wybierz artykuł: </label>
+            <select name="towary">
+                <option value="Zeszyt 60 kartek">Zeszyt 60 kartek</option>
+                <option value="Zeszyt 32 kartek">Zeszyt 32 kartek</option>
+                <option value="Cyrkiel">Cyrkiel</option>
+                <option value="Linijka 30 cm">Linijka 30 cm</option>
+            </select>
+            <br>
+            <label>liczba sztuk: </label>
+            <input type="number" name="liczba">
+            <br>
+            <input type="submit" value="OBLICZ">
+        </form>
+    </section>
+    <section id="prawy">
+        
+    </section>
+    <footer>
+    
+    </footer>
+</body>
+</html>
