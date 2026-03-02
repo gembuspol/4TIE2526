c12381c75b666f5ea89ad39a55c8f72db5ca11fd
Przemyslaw Gembalczyk <gembuspol@interia.pl>
2026-03-02 15:09:46 +0100

    a

---
+++ 23st4/wycieczki.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        Wycieczki po Europie
+    </title>
+    <link rel="stylesheet" href="style4.css">
+</head>
+<body>
+    <header>
+        <h1>BIURO TURYSTYCZNE</h1>
+    </header>
+    <section id="dane">
+        <h3>Wycieszki, na które są wolne miejsca</h3>
+        <ul>
+            <!-- tutaj pojawi się skrypt -->
+            <li></li>
+        </ul>
+    </section>
+    <section id="lewy">
+        <h2>Bestselery</h2>
+        <table>
+            <tr>
+                <td>Wenecja</td>
+                <td>kwiecień</td>
+            </tr>
+            <tr>
+                <td>Londyn</td>
+                <td>lipiec</td>
+            </tr>
+            <tr>
+                <td>Rzym</td>
+                <td>wrzesień</td>
+            </tr>
+        </table>
+    </section>
+    <section id="srodkowy">
+        <h2>Nasze zdjęcia</h2>
+        <!-- tutaj pojawi się skrypt 2 -->
+    </section>
+    <section id="prawy">
+        <h2>Skontaktuj się</h2>
+        <a href="mailto:turysta@wycieczki.pl">napisz do nas</a>
+        <p>telefon: 11222333</p>
+    </section>
+    <footer>
+        <p>Stronę wykonał Przemek</p>
+    </footer>
+
+</body>
+</html>
