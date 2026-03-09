56460c023ed743c051c6bc86d84949fd69c0f1e1
Przemyslaw Gembalczyk <gembuspol@interia.pl>
2026-03-09 15:04:21 +0100

    a

---
+++ 23st5/organizer.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sierpniowy kalendarz</title>
+    <link rel="stylesheet" href="styl5.css">
+</head>
+<body>
+    <header id="baner1"><h1>Organizer: SIERPIEŃ</h1></header>
+    <header id="baner2">
+        <form method="POST" action="organizer.html" >
+            <label for="fname">Nazwa wydarzenia</label><br>
+            <input type="text" name="fname" id="fname">
+            <input type="submit" value="OK">
+        </form>
+    </header>
+    <header id="baner3">
+        <img src="logo2.png" alt="Sierpien" width='200px' height='150px'>
+        <br>
+    </header>
+    <main>
+        
+    </main>
+    <footer><p>Strone wykonał: marcin M </p>    </footer>
+</body>
+</html>
