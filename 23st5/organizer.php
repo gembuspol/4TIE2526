df5a17fb0f87a12fa101d07ea10c62eabc7d81cf
Przemyslaw Gembalczyk <gembuspol@interia.pl>
2026-03-12 10:14:20 +0100

    a

--- 23st5/organizer.php
+++ 23st5/organizer.php
@@ -9,7 +9,7 @@
 <body>
     <header id="baner1"><h1>Organizer: SIERPIEŃ</h1></header>
     <header id="baner2">
-        <form method="POST" action="organizer.html" >
+        <form method="POST" action="organizer.php" >
             <label for="fname">Nazwa wydarzenia</label><br>
             <input type="text" name="fname" id="fname">
             <input type="submit" value="OK">
@@ -20,7 +20,27 @@
         <br>
     </header>
     <main>
-        
+        <?php
+            $polaczenie=mysqli_connect("localhost","root","","kalendarz");
+            if(isset($_POST["fname"])){
+                $wpis=$_POST["fname"];
+                $sql="UPDATE zadania SET wpis='".$wpis."' WHERE dataZadania='2020-08-09';";
+                $zapytanie=mysqli_query($polaczenie,$sql);
+            }
+            $sql1="SELECT dataZadania,wpis FROM zadania WHERE miesiac='sierpien';";
+            $zapytanie=mysqli_query($polaczenie,$sql1);
+            while($wiersz=mysqli_fetch_array($zapytanie)){
+                echo "<section class='kalendarz'>";
+                echo "<h5>";
+                echo $wiersz['dataZadania'];
+                echo "</h5>";
+                echo "<p>";
+                echo $wiersz["wpis"];
+                echo "</p>";
+                echo "</section>";
+            }
+            mysqli_close($polaczenie);
+        ?>
     </main>
     <footer><p>Strone wykonał: marcin M </p>    </footer>
 </body>
